047a515485876cf9ecde9e07e01fe6708f8f4fb2
Safa <sfalaqi@gmail.com>
2021-10-18 13:38:11 +0300

    ArrayList Activity

--- app/src/main/java/com/example/bonusapps/MainActivity.kt
+++ app/src/main/java/com/example/bonusapps/MainActivity.kt
@@ -13,12 +13,16 @@ class MainActivity : AppCompatActivity() {
         getSupportActionBar()!!.setBackgroundDrawable(AppCompatResources.getDrawable(this, R.drawable.custom_button))
         val btCounter= findViewById<Button>(R.id.btCounter)
         val btPres= findViewById<Button>(R.id.btPres)
+        val btArray= findViewById<Button>(R.id.btArray)
 
         btCounter.setOnClickListener{
           startActivity(Intent(this,ButtonCounter::class.java))
         }
         btPres.setOnClickListener{
             startActivity(Intent(this,PersistentCalculation::class.java))
+        }
+        btArray.setOnClickListener{
+            startActivity(Intent(this,ArrayListActivity::class.java))
         }
     }
 }
